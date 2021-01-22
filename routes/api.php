240bc2cac6be14b44233f2762c21c3510a1f7996
JYL <you@example.competer0512lee@gmail.com>
2021-01-22 12:02:17 +0000

    add test, controller, seeder

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\CollectionController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,39 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/collection/{id}', function (Request $request) {
-    if ($request->header('X-Secure-Code') != '1234') {
-        return response(["error" => "Secure not valid"], 403);
-    }
-    
-    return [
-        "id" => 0,
-        "title" => "親子步道",
-        "subTitle" => "老少咸宜，生活好去處",
-        "bgColor" => "yellow",
-        "iconImage" => "https://example.com/uploads/images/collection/0/icon.png",
-        "trails" => [
-            [
-                "id" => 0,
-                "title" => "冷水坑步道",
-                "location" => "台北市士林區",
-                "distance" => 600,
-                "coverImage" => "https://example.com/uploads/images/trail/0/cover.jpg"
-            ],
-            [
-                "id" => 1,
-                "title" => "東吳大學登山步道",
-                "location" => "台北市士林區",
-                "distance" => 3824,
-                "coverImage" => "https://example.com/uploads/images/trail/1/cover.jpg"
-            ],
-            [
-                "id" => 2,
-                "title" => "二子坪步道",
-                "location" => "新北市三芝區",
-                "distance" => 3600,
-                "coverImage" => "https://example.com/uploads/images/trail/2/cover.jpg"
-            ]
-        ]
-    ];
-});
+Route::resource('collection', CollectionController::class);
